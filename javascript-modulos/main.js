3b243e159afa0bda4252d4358aeb643afe2922c0
livehass <silva.felipe12@hotmail.com>
2023-05-28 13:30:05 -0300

    importa realine sync

--- javascript-modulos/main.js
+++ javascript-modulos/main.js
@@ -1,11 +1,1 @@
-if (50 > 100) {
-	console.log("Falta um tanto");
-  } else {
-	if ("1" == "-1") {
-	  console.log("Quase lá");    
-	}
-  
-	if (!null) {
-	  console.log("Sucesso");
-	}
-  }
+const input = require('readline-sync');
