4abff5f9b03ac57545bef23ccf7eef92c7309534
livehass <silva.felipe12@hotmail.com>
2023-05-28 16:07:40 -0300

    rename js

--- javascript-modulos/lacos-condicionais.js
+++ javascript-modulos/lacos-condicionais.js
@@ -1,9 +1,27 @@
-const input = require('readline-sync')
+// const input = require('readline-sync')
 
-let nota;
-let soma = 0;
-for (let i = 1; i <= 3; i++){
-    nota = Number(input.question(`informe sua nota ${i} do aluno:`))
-    soma = soma + nota
+// let nota;
+// let soma = 0;
+// for (let i = 1; i <= 3; i++){
+//     nota = Number(input.question(`informe sua nota ${i} do aluno:`))
+//     soma = soma + nota
+// }
+// console.log(`A média do aluno é ${soma / 3}.`);
+
+
+let arr = ['felipe', 26, 1.85, true]
+
+console.log(arr[0])
+console.log(arr.length)
+
+for (let i = 0; i < arr.length; i++){
+    console.log(typeof i + i)
 }
-console.log(`A média do aluno é ${soma / 3}.`);
+console.clear()
+for (let e of arr){        //elemento
+    console.log(e)
+}
+console.clear()
+for (let indice in arr){ //indice
+    console.log(arr[indice])
+}
